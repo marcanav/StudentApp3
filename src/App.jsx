19d869781a6015b39a7746079b7810eb5be50244
Maria Nav <mnavar17@my.centennialcollege.ca>
2024-04-04 22:48:06 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "ecomm/Header";
+
+import "./index.css";
+
+const App = () => (
+  <div className="container">
+    <Header />
+    Student App 3
+
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
